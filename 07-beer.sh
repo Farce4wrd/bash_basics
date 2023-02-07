93de0a6cf18814f195dde4babc492bb2d7a0258c
Farce4wrd <ipeterokolocha@gmail.com>
2023-02-07 16:08:38 -0500

    Added new counting song

--- 07-beer.sh
+++ 07-beer.sh
@@ -22,3 +22,24 @@ done
 
 # exercise: implement another counting song (such as 12 days of Christmas) 
 # using loops and if statements.
+echo "How about another song, Murray?"
+echo "How many monkeys jumping on the bed?"
+read counts
+
+while [ $counts -ge 0 ]; do
+  if [ $counts -ge 2 ]; then
+      echo "$counts little monkeys jumping on the bed, One fell down and bumped his head"
+      echo "Mama called the doctor and the doctor said,"
+      echo "No more monkeys jumping on the bed!"
+  elif [ $counts -eq 1 ]; then
+      echo "$count little monkey jumping on the beds, She fell down and bumped her head"
+      echo "Mama called the doctor and the doctor said,"
+      echo "Put those monkeys back to bed!"
+  else
+      echo "no more monkeys jumping on the bed"
+  fi
+  
+  # the following statement is equivalent to: let "count=count-1"
+  ((counts = counts - 1))
+
+done
